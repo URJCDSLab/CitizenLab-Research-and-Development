b27880978a65b5fff374809027b12173fd6fef3d
alexgarcia32 <62252675+alexgarcia32@users.noreply.github.com>
2023-06-16 15:45:13 +0200

    Update cu_25_step_06_script.R

--- casos_urjc/notebooks/V_model_operation/25_listas_espera/cu_25_step_06_script.R
+++ casos_urjc/notebooks/V_model_operation/25_listas_espera/cu_25_step_06_script.R
@@ -1,6 +1,5 @@
 ## PROCESO PREDICCIÓN Y SIMULACIÓN MASIVA
 
-
 library(readr)
 library(tidyverse)
 library(tidymodels)
@@ -9,6 +8,28 @@ library(modeltime)
 library(simmer)
 library(simmer.plot)
 library(tidyr)
+Sys.setlocale(category = "LC_ALL", locale = "es_ES.UTF-8")
+
+
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if(length(args) < 3){
+  stop("Se deben especificar tres argumentos: el primero para la carpeta de entrada, el segundo para la carpeta de salida y el tercero para la carpeta de maestros")
+} else if(!all(file.exists(args[1]),
+               file.exists(args[2]),
+               file.exists(args[3]))){
+  stop("Alguna de las carpetas especificadas no existe, compruebe por favor.")
+} else{
+  carpeta_entrada <- args[1]
+  carpeta_salida <- args[2]
+  carpeta_maestros <- args[3]
+  message("\n1: Carpetas correctas: ", 
+          paste0(carpeta_entrada, carpeta_salida, carpeta_maestros,
+                 collapse = ", "),
+          "\n")
+}
+
 
 ## EN CASO DE QUE LA PREDICCIÓN AL FINAL DEL HORIZONTE RESULTE EN UN NÚMERO NEGATIVO, SE TOMA EL
 ## ÚLTIMO VALOR POSITIVO (O LO QUE ES LO MISMO, SE ELIMINAN LOS NEGATIVOS)
@@ -39,7 +60,12 @@ params_sim <- function(s, periodo, h){
       slice_max(fecha) |> 
       pull(.actual)
     
-    
+    if (length(pacientes_en_cola) == 0) {
+      pacientes_en_cola <- 0
+    }
+    if (length(tiempo_medio_en_cola) == 0) {
+      tiempo_medio_en_cola <- 0
+    }
     
   } else{
     
@@ -50,6 +76,14 @@ params_sim <- function(s, periodo, h){
     tiempo_medio_en_cola <- pred_h_tiempo |> 
       filter(.value > 0) |> 
       slice_tail(n = 1) |> pull(.value)
+
+
+    if (length(pacientes_en_cola) == 0) {
+      pacientes_en_cola <- 0
+    }
+    if (length(tiempo_medio_en_cola) == 0) {
+      tiempo_medio_en_cola <- 0
+    }
   }
   
   if(pacientes_en_cola > 0 & tiempo_medio_en_cola > 0){
@@ -70,12 +104,12 @@ params_sim <- function(s, periodo, h){
 
 ## Datos y modelos
 
-modelo_pacientes <- read_rds("cu_25_maestros/modelos_pacientes_xgboost.rds")
-modelo_tiempo <- read_rds("cu_25_maestros/modelos_tiempo_xgboost.rds")
+modelo_pacientes <- read_rds(paste0(carpeta_maestros, "/modelos_pacientes_xgboost.rds"))
+modelo_tiempo <- read_rds(paste0(carpeta_maestros, "/modelos_tiempo_xgboost.rds"))
 
-capacidad <- read_csv("cu_25_step_01_input/CU_25_05_07_01_capacidad.csv")
+capacidad <- read_csv(paste0(carpeta_entrada, "/CU_25_05_07_01_capacidad.csv"))
 
-variables <- read_csv("cu_25_step_01_input/VARIABLES.csv") 
+variables <- read.csv(paste0(carpeta_entrada, "/VARIABLES.csv")) 
 
 df <- data.frame(s = names(modelo_pacientes)) |> 
   tidyr::separate(s, c("nombre_area", "Especialidad"), "\\.", remove = FALSE) |> 
@@ -86,15 +120,15 @@ df <- data.frame(s = names(modelo_pacientes)) |>
 ## COGER DE VARIABLES !!
 H <- variables |> 
   filter(variable == "HORIZONTE") |> 
-  pull(valor) |> 
-  as.numeric()
+  pull(valor) 
 
 # H <- 2
 NPER <- variables |> 
   filter(variable == "NPER") |> 
-  pull(valor)
+  pull(valor) |> as.numeric()
 
 #simulación
+message("Simulación según último valor")
 l_sim_ultimo <- df |> 
   # slice(1:2) |>
   pull(s) |> 
@@ -121,9 +155,12 @@ l_sim_ultimo <- df |>
     recursos <- get_mon_resources(env)
     llegadas <- get_mon_arrivals(env, ongoing = TRUE)
     return(list(recursos = recursos |> mutate(id = .x),
-                llegadas = llegadas |> mutate(id = .x)))
+                llegadas = llegadas |> mutate(id = .x),
+                pred_h_personas = pars$pred_h_personas |> mutate(id = .x),
+                pred_h_tiempo = pars$pred_h_tiempo |> mutate(id = .x)))
     
-  }) 
+  }, .progress = TRUE) 
+message("Simulación según horizonte")
 l_sim_h <- df |> 
   # slice(1:2) |>
   pull(s) |> 
@@ -151,10 +188,10 @@ l_sim_h <- df |>
     llegadas <- get_mon_arrivals(env, ongoing = TRUE)
     return(list(recursos = recursos |> mutate(id = .x),
                 llegadas = llegadas |> mutate(id = .x),
-                pred_h_personas = pars$pred_h_personas,
-                pred_h_tiempo = pars$pred_h_tiempo))
+                pred_h_personas = pars$pred_h_personas |> mutate(id = .x),
+                pred_h_tiempo = pars$pred_h_tiempo |> mutate(id = .x)))
     
-  }) 
+  }, .progress = TRUE) 
 
 
 
@@ -169,8 +206,24 @@ pred_h_tiempo <- l_sim_h |> map_dfr(~.x |> pluck("pred_h_tiempo"))
 pred_h_personas <- l_sim_h |> map_dfr(~.x |> pluck("pred_h_personas"))
 
 
-write_rds(sim_ultimo_recursos, "cu_25_step_06_output/sim_ultimo_recursos.rds")
-write_rds(sim_ultimo_llegadas, "cu_25_step_06_output/sim_ultimo_llegadas.rds")
-write_rds(sim_h_recursos, "cu_25_step_06_output/sim_h_recursos.rds")
-write_rds(pred_h_tiempo, "cu_25_step_06_output/pred_h_tiempo.rds")
-write_rds(pred_h_personas, "cu_25_step_06_output/pred_h_personas.rds")
+write_rds(sim_ultimo_recursos, paste0(carpeta_salida,"/sim_ultimo_recursos.rds"))
+write_rds(sim_ultimo_llegadas, paste0(carpeta_salida,"/sim_ultimo_llegadas.rds"))
+write_rds(sim_h_recursos, paste0(carpeta_salida,"/sim_h_recursos.rds"))
+write_rds(pred_h_tiempo, paste0(carpeta_salida,"/pred_h_tiempo.rds"))
+write_rds(pred_h_personas, paste0(carpeta_salida,"/pred_h_personas.rds"))
+write_rds(sim_h_llegadas, paste0(carpeta_salida,"/sim_h_llegadas.rds"))
+
+
+## Copiar resto input a output para siguientes pasos
+file.copy(paste0(carpeta_entrada, "/CU_25_05_03_areasgeo.json"),
+          paste0(carpeta_salida, "/CU_25_05_03_areasgeo.json"))
+file.copy(paste0(carpeta_entrada, "/CU_25_05_05_01_hospitales.csv"),
+          paste0(carpeta_salida, "/CU_25_05_05_01_hospitales.csv"))
+file.copy(paste0(carpeta_entrada, "/CU_25_05_06_indicadores_area.csv"),
+          paste0(carpeta_salida, "/CU_25_05_06_indicadores_area.csv"))
+file.copy(paste0(carpeta_entrada, "/CU_25_05_07_01_capacidad.csv"),
+      paste0(carpeta_salida, "/CU_25_05_07_01_capacidad.csv"))
+file.copy(paste0(carpeta_entrada, "/CU_25_05_07_02_lista_espera.csv"),
+      paste0(carpeta_salida, "/CU_25_05_07_02_lista_espera.csv"))
+    file.copy(paste0(carpeta_entrada, "/VARIABLES.csv"),
+      paste0(carpeta_salida, "/VARIABLES.csv"))
