e636c4242eea39a26b7414af614514c085871e8f
emilopezcano <emilopezcano@gmail.com>
2023-06-19 17:33:55 +0200

    cu 18 paso 04 completar cosas que faltaban

--- casos_urjc/notebooks/V_model_operation/18_infraestructuras/cu_18_step_04_app.R
+++ casos_urjc/notebooks/V_model_operation/18_infraestructuras/cu_18_step_04_app.R
@@ -9,6 +9,8 @@ library(DT, warn.conflicts = FALSE)
 library(leaflet)
 library(ggplot2)
 library(plotly, warn.conflicts = FALSE)
+library(gridExtra)
+library(mclust)
 # if (!require(mapSpain)) {
 #     install.packages("mapSpain")
 # }
@@ -25,43 +27,39 @@ COL1 <- rgb(33/255, 150/255, 243/255)
 
 # UI
 ui <- function(request) {
-
-  fluidPage(
-
-    theme = bs_theme(bootswatch = "flatly"),
-
-    useShinydashboard(),
-
-    titlePanel(title = "Visualización - CitizenLab CU 18"),
-
-
-
-
-    # ... Otros elementos de la UI
-
-   
-
   
-
-
-
-      mainPanel(
-
-          tabPanel("Visualización",
-
-                   column(width = 12,
-
-                          leafletOutput("map")
-
-
-          
-
+  fluidPage(
+    
+    theme = bs_theme(bootswatch = "flatly"),
+    
+    useShinydashboard(),
+    
+    titlePanel(title = "Modelos CLUSTER - CitizenLab CU 18"),
+    
+    # ... Otros elementos de la UI
+    
+    mainPanel(
+      tabsetPanel(
         
-
-      
-
-    ))))
-
+        tabPanel("Mapa",
+                 
+                 column(width = 12,
+                        uiOutput("uiadp_fecha"),
+                        leafletOutput("map"))),
+        tabPanel("Gráfico",
+                 column(width = 12,
+                        plotOutput("pcluster", width = "80%",
+                                   height = "600px"))
+        ),
+        tabPanel("Escenario",
+                 column(width = 12,
+                        DT::dataTableOutput("tescenario"))),
+        tabPanel("Tabla",
+                 column(width = 12,
+                        DT::dataTableOutput("tcluster")))
+        
+      )))
+  
 }
 
 # SERVER
@@ -116,14 +114,13 @@ server <- function(input, output, session) {
   observeEvent(input$error_carpetas_existen,{
     stopApp()
   })
-
+  
   ## Read variable data
   dfvariables <- reactive({
-    print(carpetas()$carpeta_entrada)
     read_csv(paste0(carpetas()$carpeta_entrada, "/VARIABLES.csv"), 
              show_col_types = FALSE)
   })
-
+  
   datos_distritos <- reactive({
     read_csv(file.path(carpetas()$carpeta_entrada, "/CU_18_05_16_distritos_variables.csv"))
   })
@@ -131,11 +128,11 @@ server <- function(input, output, session) {
   datos_infra <- reactive({
     read_csv(file.path(carpetas()$carpeta_entrada, "/CU_18_05_19_01_infraestructuras.csv"))
   })
-
+  
   datos_diario <- reactive({
     read_csv(file.path(carpetas()$carpeta_entrada, "/CU_18_05_20_diario_infra.csv"))
   })
-
+  
   distritos_geojson <- reactive({
     read_sf(file.path(carpetas()$carpeta_entrada, "/CU_18_05_03_distritos_geo.json"))
   })
@@ -145,64 +142,140 @@ server <- function(input, output, session) {
     read_rds(file.path(carpetas()$carpeta_maestros,"/datos_cluster_diario.rds"))})
   
   datos_cluster_distritos <- reactive({
-    read_rds(file.path(carpetas()$carpeta_maestros,"/datos_cluster_distritos.rds"))})
-
-
+    read_rds(file.path(carpetas()$carpeta_maestros,"/datos_cluster_distritos.rds"))
+  })
+  
+  modelo_cluster <- reactive({
+    if (dfvariables() |> filter(variable == "NIVEL") |> pull(valor) == "Distrito") {
+      read_rds(file.path(carpetas()$carpeta_maestros, "/modelo_cluster_distritos.rds"))
+    }else{
+      read_rds(file.path(carpetas()$carpeta_maestros, "/modelo_cluster_diario.rds"))
+    }
+  })
+  
+  ## ESCENARIO
+  
+  datos_escenario <- reactive({
+    if (dfvariables() |> filter(variable == "NIVEL") |> pull(valor) == "Distrito") {
+      read_csv(file.path(carpetas()$carpeta_entrada, "/ESCENARIO_CLUSTER_DIST.csv"))
+    } else{
+      read_csv(file.path(carpetas()$carpeta_entrada, "/ESCENARIO_CLUSTER_DIARIO.csv"))
+    }
+  })
+  
+  ## Selector de fecha para mapa
+  
+  output$uiadp_fecha <- renderUI({
+    
+    ff <- datos_diario() |> pull(fecha)
+    
+    airDatepickerInput("adp_fecha", label = "Fecha", value = max(ff), 
+                       minDate = min(ff),
+                       maxDate = max(ff))
+  })
+  
+  
   ## Render leaflet map
   output$map <- renderLeaflet({
-    map <- leaflet() %>%
-      addTiles()
+    req(input$adp_fecha)
+    # map <- leaflet() %>%
+    #   addTiles()
     
     if (dfvariables() |> filter(variable == "NIVEL") |> pull(valor) == "Distrito") {
-
-dmap <- distritos_geojson() |> # slice(1:100) |>
-inner_join(datos_cluster_distritos(), by = c("CDIS" = "cdis"), multiple="all") 
-
-
-colors <- colorFactor("RdYlBu", unique(dmap$cluster))
-
-dmap |> leaflet() |>
-  addTiles() |>
-  addPolygons(color="black", 
-    fillColor = ~colors(cluster),
-
-              fillOpacity = 0.8,
-
-              weight = 1,
-
-              popup = ~paste("Distrito", CDIS) 
-  ) |>
-  addLegend("bottomright", values = ~cluster,
-  pal=colors,
-        # colors=~colors(unique(cluster)),
-
-            title = "Clusters",
-
-            opacity = 1
-
-  ) 
-  }else{ 
+      
+      dmap <- distritos_geojson() |> # slice(1:100) |> 
+        inner_join(datos_cluster_distritos() , by = c("CDIS" = "cdis",
+                                                      "CMUN" = "cmun"), multiple="all")
+      colors <- colorFactor("RdYlBu", unique(dmap$cluster))
+      
+      dmap |> leaflet() |>
+        addTiles() |>
+        addPolygons(color="black", 
+                    fillColor = ~colors(cluster),
+                    
+                    fillOpacity = 0.8,
+                    
+                    weight = 1,
+                    popup = leafpop::popupTable(dmap,
+                                                c("hospitales", "centros_de_salud",
+                                                  "estaciones_de_cercanias")),
+                    label = ~paste("Distrito", CDIS, "Municipio", CMUN)
+        ) |>
+        addLegend("bottomright", values = ~cluster,
+                  pal=colors,
+                  # colors=~colors(unique(cluster)),
+                  
+                  title = "Clusters",
+                  
+                  opacity = 1
+                  
+        ) 
+    }else{ 
+      
+      dmap <- datos_infra() |>
+        inner_join(datos_cluster_diario()|>
+                     filter(fecha == input$adp_fecha),
+                   by = c("id_inf" = "id_inf"), multiple="all") 
+      
+      
+      
+      colors <- colorFactor("RdYlBu", unique(dmap$cluster))
+      
+      map <- leaflet(dmap) %>%
+        addTiles() %>%
+        addCircleMarkers(
+          ~X, ~Y, # longitude and latitude
+          color = ~colors(cluster), # Color based on "puntos" column
+          radius = 5, # Adjust this based on your requirements
+          stroke = FALSE, fillOpacity = 0.8,
+          label = ~paste0(nombre),
+          popup = ~paste0("<b>Tipo</b>: ", tipo, "<br/><b>Grupo: </b>", grupo)
+        ) %>%
+        addLegend("bottomright", pal = colors, values = ~cluster,
+                  title = "cluster",
+                  opacity = 1)
+    }})
+  
+  
+  output$pcluster <- renderPlot({
+    if (dfvariables() |> filter(variable == "NIVEL") |> pull(valor) == "Distrito") {
+      gdata <- datos_cluster_distritos()
+    } else{
+      gdata <- datos_cluster_diario()
+    }
+    p1 <- gdata |> ggplot(aes(x = Dim.1, y = Dim.2, col = cluster)) +
+      geom_point(alpha = 0.5)
+    p2 <- gdata |> ggplot(aes(x = Dim.2, y = Dim.3, col = cluster)) +
+      geom_point(alpha = 0.5)
+    p3 <- gdata |> ggplot(aes(x = Dim.1, y = Dim.3, col = cluster)) +
+      geom_point(alpha = 0.5)
     
-    dmap <- datos_infra() |>
-    inner_join(datos_cluster_diario(), by = c("id_inf" = "id_inf"), multiple="all") 
-
-    colors <- colorFactor("RdYlBu", unique(dmap$cluster))
-
-    map <- leaflet(dmap) %>%
-            addTiles() %>%
-            addCircleMarkers(
-                ~X, ~Y, # longitude and latitude
-                color = ~colors(cluster), # Color based on "puntos" column
-                radius = 5, # Adjust this based on your requirements
-                stroke = FALSE, fillOpacity = 0.8
-                # label = ~paste0(nombre, " Turistas")
-            ) %>%
-            addLegend("bottomright", pal = colors, values = ~cluster,
-                      title = "cluster",
-                      opacity = 1)
-  }})
-
-
+    p <- grid.arrange(p1, p2, p3)
+    
+    
+  })
+  output$tcluster <- DT::renderDataTable({
+    if (dfvariables() |> filter(variable == "NIVEL") |> pull(valor) == "Distrito") {
+      tdata <- datos_cluster_distritos()
+      tcols <- 9:146
+    } else{
+      tdata <- datos_cluster_diario()
+      tcols <- 9:15
+    }
+    print(tdata)
+    tdata |> 
+      group_by(cluster) |> 
+      summarise(n = n(),
+                across(tcols, ~mean(.x, na.rm = TRUE))) |> 
+      datatable(options = list(scrollX = TRUE))
+  })
+  
+  output$tescenario <- DT::renderDataTable({
+      datos_escenario() |> 
+        mutate(Cluster = predict(modelo_cluster(), datos_escenario())$classification,
+               .before = 1) |> 
+        datatable(options = list(scrollX = TRUE))
+  })
 }
 
 shinyApp(ui, server)
